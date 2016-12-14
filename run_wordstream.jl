c2e1e1c9bedb1e78ca7c5beced32aff393b7b2b2
David Little <david.frank.little@gmail.com>
2016-12-14 09:48:45 -0800

    Added files to ignore.

--- run_wordstream.jl
+++ run_wordstream.jl
@@ -1,3 +1,12 @@
+# NEW NOTES# pitch changes
+# more chunks of the same sound
+# maybe introduce to other VTs
+# do we have people respond to the most recent stimulus rather than all 4
+# or does it split at all
+# do we run as a block
+# allow restarting of expeirment
+# make shorter? or make sure 1st half has everything
+
 # NOTES:
 
 # concern that there is a delay in when you hear a stream switch and when you
@@ -6,63 +15,74 @@
 
 include("util.jl")
 using Psychotask
-using Lazy: @_, @>
-
-# make sure that play is fully compiled
-# play(silence(0.1))
-play(tone(1000,1))
-
-sid = (length(ARGS) > 0 ? ARGS[1] : "test_sid")
- 
-sr = 44100.0
-s_sound = loadsound("sounds/s_stone.wav")
-dohne_sound = loadsound("sounds/dohne.wav")
-dome_sound = loadsound("sounds/dome.wav")
-drun_sound = loadsound("sounds/drun.wav")
-drum_sound = loadsound("sounds/drum.wav")
-
-function withgap(a,b,gap)
-  sound(attenuate(mix(a,[silence(duration(a)+gap); b]),20))
-end
-
+using Lazy: @>
 const ms = 1/1000
-
-stimuli = Dict(
-  (:normal,   :w2nw) => withgap(s_sound,dohne_sound,41ms),
-  (:negative, :w2nw) => withgap(s_sound,dohne_sound,-100ms),
-  (:normal,   :nw2w) => withgap(s_sound,dome_sound,41ms),
-  (:negative, :nw2w) => withgap(s_sound,dome_sound,-100ms),
-  (:normal,   :w2w) => withgap(s_sound,drum_sound,41ms),
-  (:negative, :w2w) => withgap(s_sound,drum_sound,-100ms),
-  (:normal,   :nw2nw) => withgap(s_sound,drun_sound,41ms),
-  (:negative, :nw2nw) => withgap(s_sound,drun_sound,-100ms)  
-)
-
-cross = render_text("+")
-break_text = render_text("You can take a break. Hit"*
-                         " any key when you're ready to resume...")
+atten_dB = 20
+play(attenuate(ramp(tone(1000,1)),atten_dB))
 
 # We might be able to change this to ISI now that there
 # is no gap.
 SOA = 672.5ms
 response_timeout = 750ms
 trial_pause = 100ms
-n_trials = 80
-n_break_after = 10
+n_trials = 40
+n_break_after = 5
 n_repeat_example = 2
-stimuli_per_response = 4
-responses_per_phase = 6
+stimuli_per_response = 6
+responses_per_phase = 8
+normal_s_gap = 41ms
+negative_gap = -100ms
 
-context_types = [:normal,:negative]
-word_types = [:w2nw,:nw2w,:w2w,:nw2nw]
+s_stone = loadsound("sounds/s_stone.wav")
+dohne = loadsound("sounds/dohne.wav")
+dome = loadsound("sounds/dome.wav")
+drun = loadsound("sounds/drun.wav")
+drum = loadsound("sounds/drum.wav")
 
-words,contexts = @> [(w,c) for w in word_types for c in context_types][:] begin
+# what is the dB difference between the s and sthe dohne?
+rms(x) = sqrt(mean(x.^2))
+dB_s = -20log10(rms(s_stone) / rms(dohne))
+
+function withgap(a,b,gap)
+  sound(mix(attenuate(a,atten_dB+dB_s),[silence(duration(a)+gap); attenuate(b,atten_dB)]))
+end
+
+# maybe block
+
+stimuli = Dict(
+  (:normal,   :w2nw) => withgap(s_stone,dohne,normal_s_gap),
+  (:negative, :w2nw) => withgap(s_stone,dohne,negative_gap),
+  (:normal,   :nw2w) => withgap(s_stone,dome,normal_s_gap),
+  (:negative, :nw2w) => withgap(s_stone,dome,negative_gap),
+  (:normal,   :w2w) => withgap(s_stone,drum,normal_s_gap),
+  (:negative, :w2w) => withgap(s_stone,drum,negative_gap),
+  (:normal,   :nw2nw) => withgap(s_stone,drun,normal_s_gap),
+  (:negative, :nw2nw) => withgap(s_stone,drun,negative_gap)
+)
+
+# randomize presentations, but gaurantee that all stimuli
+# are presented in equal quantity within the first and second half
+# of trials
+contexts1,words1 = @> keys(stimuli) begin
   cycle
-  take(n_trials)
+  take(div(n_trials,2))
   collect
   shuffle
   unzip
 end
+
+contexts2,words2 = @> keys(stimuli) begin
+  cycle
+  take(n_trials - div(n_trials,2))
+  collect
+  shuffle
+  unzip
+end
+
+contexts = [contexts1; contexts2]
+words = [words1; words2]
+
+sid = (length(ARGS) > 0 ? ARGS[1] : "test_sid")
 
 function syllable(spacing,stimulus,phase)
   sound = stimuli[spacing,stimulus]
@@ -76,11 +96,10 @@ end
 
 # TODO: maybe make this a standard construct in Psychotask.
 function instruct(str)
+  text = render(str*" (Hit spacebar to continue...)")
   m = moment() do t
-    clear()
     record("instructions",time=t)
-    draw(str*"\n\n(Hit spacebar to continue...)")
-    display()
+    display(text)
   end
   [m,response(iskeydown(key":space:"))]
 end
@@ -118,26 +137,23 @@ exp = Experiment(condition = "pilot",sid = sid,version = v"0.0.5") do
   # TODO: show cross could also be a simpler primitive e.g. cross(pause =
   # trial_pause)
   # TODO: trials should be able to flatten out nested lists, to simplify syntax
-  show_cross = moment(trial_pause) do t
-    clear()
-    draw(cross)
-    display()
-  end
-  
+  cross = render("+")
+  show_cross = moment(trial_pause,t -> display(cross))
+
   addbreak(
     instruct("""
 
-      In each trial of the present experiment you will listen to the same
-      syllable repeated over and over. Over time the sound of this sylable
-      may (or may not) appear to change.""")...,
+      In each trial of the present experiment you will listen to the same word
+      or a non-word repeated over and over. Over time the sound of this word or
+      non-word may (or may not) appear to change."""),
     instruct("""
 
       For example the word "stone" may begin to sound like an "s" that is
-      separate from a second, "dohne" sound. See if you can hear the word
-      "stone" change to the sound "dohne" in the following example.""")...)
+      separate from a second, "dohne" sound. See if you can hear the sound
+      "stone" change to the sound "dohne" in the following example."""))
 
   addtrial(blank,show_cross,
-           repeated(syllable(:normal,:w2nw,"example"),n_repeat_example)...,
+           repeated(syllable(:normal,:w2nw,"example"),n_repeat_example),
            moment(SOA))
 
   x = stimuli_per_response
@@ -145,36 +161,52 @@ exp = Experiment(condition = "pilot",sid = sid,version = v"0.0.5") do
     instruct("""
 
       In this experiment we'll be asking you to listen for whether it appears
-      that the begining "s" of a sound is a part of the following syllable or
-      separate from it.""")...,
+      that the begining "s" of a sound is a part of the following sound or
+      separate from it."""),
     instruct("""
 
-      So, for example, if the syllable presented is "stone" we
+      So, for example, if the word presented is "stone" we
       want to know if you hear "stone" or "dohne". There may be
-      other changes to the sound that you hear; please ignore them.""")...,
+      other changes to the sound that you hear; please ignore them."""),
     instruct("""
 
       After every $x sounds, we want you to indicate what you heard most
       often. Let's practice a bit.  Use "Q" to indicate that you heard the "s"
       as part of the sound most of the time, and "P" otherwise.  Respond as
-      promptly as you can.""")...)
+      promptly as you can.""") )
 
+  # TODO: record responses
+  isresponse(e) = iskeydown(e,key"p") || iskeydown(e,key"q")
   stims = repeated(syllable(:normal,:w2nw,"practice"),stimuli_per_response)
-  resp = response(e -> iskeydown(e,key"q") || iskeydown(e,key"p"))
+  resp = response(isresponse)
   x = [blank,show_cross,stims...,resp]
-  # addtrial(take(cycle(x),length(x)*responses_per_phase)...)
+  addtrial(take(cycle(x),length(x)*responses_per_phase) )
 
   addbreak(instruct("""
+  
+    In the real experiment, your time to respond will be limited. Let's
+    try another practice round, this time a little bit faster.
+  """) )
 
-    In the real experiment, your time to respond will be limited. Try to respond
-    before your time is up.
-    """)...)
+  go_faster = render("Faster!")
+  resp = timeout(isresponse,2response_timeout) do time
+    record("response_timeout",time=time)
+    display(go_faster)
+  end
+  x = [blank,show_cross,stims...,resp]
+  addtrial(take(cycle(x),length(x)*responses_per_phase) )  
+  
+  addbreak(instruct("""
+
+    In the real experiment, your time to respond will be even more limited. 
+    Please try to respond before you see the "Faster!" flash, but even if
+    it does flash, please still respond.
+  """) )
 
   anykey = response(e -> iskeydown(e) || endofpause(e))
+  anykeystr = render("Hit any key to start the real experiment...")
   anykey_message = moment() do t
-    clear()
-    draw("Hit any key to start the real experiment...")
-    display()
+    display(anykeystr)
   end
 
   addbreak(anykey_message,anykey)
@@ -185,13 +217,10 @@ exp = Experiment(condition = "pilot",sid = sid,version = v"0.0.5") do
     # TODO: limit time for response?
     # TODO: I can create a higher level primitive
     # that makes this easier to use: e.g. "q" => "stream_1", "p" => "stream_2"
-    go_faster = render_text("Faster!")
-    isresponse(e) = iskeydown(e,key"p") || iskeydown(e,key"q")
+    go_faster = render("Faster!")
     r = timeout(isresponse,response_timeout) do time
       record("response_timeout",time=time)
-      clear()
-      draw(go_faster)
-      display()
+      display(go_faster)
     end
 
     x = [show_cross,repeated(context,stimuli_per_response)...,r]
@@ -199,34 +228,32 @@ exp = Experiment(condition = "pilot",sid = sid,version = v"0.0.5") do
     x = [show_cross,repeated(test,stimuli_per_response)...,r]
     test_phase = take(cycle(x),length(x)*responses_per_phase)
 
-    addtrial(context_phase...,test_phase...) do event
+    addtrial(context_phase,test_phase) do event
       if iskeydown(event,key"q")
-        record("stream_1",time = event.time)
+        record("stream_1",time = time(event))
       elseif iskeydown(event,key"p")
-        record("stream_2",time = event.time)
+        record("stream_2",time = time(event))
       end
       
       if endofpause(event)
-        clear()
-        draw(cross)
-        display()
+        display(cross)
       end
     end
 
+    anykeybut = response(e -> (iskeydown(e) || endofpause(e)) &&
+                         !(iskeydown(e,key"p") || iskeydown(e,key"q")))
     # add a break after every n_break_after trials
     if i > 0 && i % n_break_after == 0
-      break_text = render_text("You can take a break. Hit"*
+      break_text = render("You can take a break. Hit"*
                                " any key when you're ready to resume..."*
                                "\n$(div(i,n_break_after)) of "*
                                "$(div(n_trials,n_break_after)) breaks.")
       message = moment() do t
         record("break")
-        clear()
-        draw(break_text)
-        display()
+        display(break_text)
       end
 
-      addbreak(message,anykey)
+      addbreak(message,anykeybut)
     end
 
   end
