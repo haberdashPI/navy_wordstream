9b8e3146ebf9fa389493260c053b5d7eeb254e2b
David Little <david.frank.little@gmail.com>
2017-01-23 14:42:10 -0800

    Created more pilot plots.

--- plot.results.R
+++ plot.results.R
@@ -4,7 +4,7 @@ library(dplyr)
 source('trial_to_times.R')
 
 file_pat = 'pilot_[0-9]+.*0.2.2_2017.*.csv'
-data = Reduce(rbind,Map(read.csv,list.files('data',file_pat,full.names=T)))
+## data = Reduce(rbind,Map(read.csv,list.files('data',file_pat,full.names=T)))
 
 by_time = data %>%
   filter(code %in% c('stimulus','stream_1','stream_2')) %>%
@@ -16,23 +16,57 @@ by_context = by_time %>%
 	group_by(sid,time,stimulus,context) %>%
 	summarise(response = mean(response-1),N = length(response))
 
-legend = guide_legend(title='Context Stimulus')
-ggplot(subset(by_context,sid == 3),
+for(num in unique(by_time$sid)){
+  legend = guide_legend(title='Context Stimulus')
+  ggplot(subset(by_context,sid == num),
+         aes(x=time,y=response,color=context,group=context)) +
+    geom_line() + facet_wrap(~stimulus) +
+    geom_hline(yintercept=0.5,linetype=2) +
+    theme_classic() + ylab('% streaming') + xlab('time (s)')
+  ggsave(paste('plots/ind_',num,'_',Sys.Date(),'.pdf',sep=''))
+}
+
+means = by_context %>%
+  filter(sid %in% c(1,3)) %>%
+  group_by(time,stimulus,context) %>%
+  summarise(response = mean(response))
+
+ggplot(means,
        aes(x=time,y=response,color=context,group=context)) +
 	geom_line() + facet_wrap(~stimulus) +
   geom_hline(yintercept=0.5,linetype=2) +
 	theme_classic() + ylab('% streaming') + xlab('time (s)')
+ggsave(paste('plots/means_',Sys.Date(),'.pdf',sep=''))
 
-by_context = by_time %>%
-	filter(response > 0,sid %in% c(1,3)) %>%
-	group_by(time,stimulus,context) %>%
+by_context1 = by_time %>%
+	filter(response > 0,trial < 32) %>%
+	group_by(sid,time,stimulus,context) %>%
 	summarise(response = mean(response-1),N = length(response))
+means1 = by_context1 %>%
+  filter(sid %in% c(1,3)) %>%
+  group_by(time,stimulus,context) %>%
+  summarise(response = mean(response))
 
-ggplot(by_context,
+ggplot(means1,
        aes(x=time,y=response,color=context,group=context)) +
 	geom_line() + facet_wrap(~stimulus) +
   geom_hline(yintercept=0.5,linetype=2) +
 	theme_classic() + ylab('% streaming') + xlab('time (s)')
+ggsave(paste('plots/means_1st_half_',Sys.Date(),'.pdf',sep=''))
 
+by_context2 = by_time %>%
+	filter(response > 0,trial >= 32) %>%
+	group_by(sid,time,stimulus,context) %>%
+	summarise(response = mean(response-1),N = length(response))
+means2 = by_context2 %>%
+  filter(sid %in% c(1,3)) %>%
+  group_by(time,stimulus,context) %>%
+  summarise(response = mean(response))
+ggplot(means2,
+       aes(x=time,y=response,color=context,group=context)) +
+	geom_line() + facet_wrap(~stimulus) +
+  geom_hline(yintercept=0.5,linetype=2) +
+	theme_classic() + ylab('% streaming') + xlab('time (s)')
+ggsave(paste('plots/means_2nd_half_',Sys.Date(),'.pdf',sep=''))
 
 #ggsave(paste('data/speech_streaming_joel_',Sys.Date(),'.pdf',sep=''))
