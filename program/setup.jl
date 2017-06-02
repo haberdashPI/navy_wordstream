3404006aba80647d89048ecf7047da726e692ef7
David Little <david.frank.little@gmail.com>
2017-06-02 09:11:45 -0700

    Updated setup

--- program/setup.jl
+++ program/setup.jl
@@ -1,11 +1,20 @@
-Pkg.add("Weber",v"0.4.4")
+Pkg.add("Weber",v"0.5.1")
 if !isfile("calibrate.jl")
   open("calibrate.jl","w") do s
-    println(s,"# call run_calibrate() to select an appropriate attenuation.")
-    println(s,"const atten_dB = 30")
-    println(s,"")
-    println(s,"# call Pkg.test(\"Weber\"). If the timing test fails, increase ")
-    println(s,"# moment resolution to avoid warnings.")
-    println(s,"const moment_resolution = 0.0015")
+    print(s,"""
+    # call run_calibrate() to select an appropriate attenuation.
+    const atten_dB = 30
+
+    # call Pkg.test(\"Weber\"). If the timing test fails, increase
+    # moment resolution to avoid warnings.
+    const moment_resolution = 1.5ms
+
+    const stream_1 = key":cedrus5:"
+    const stream_2 = key":cedrus6:"
+    const end_break_key = key"`"
+
+    # select an appropriate serial port for stimtrak
+    const stimtrak_port = nothing
+    """)
   end
 end
